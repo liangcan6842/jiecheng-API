eb986d015e864912258e7fa457b27ba264bbbb35
liangcan <1473166229@qq.com>
2022-08-22 12:58:47 +0800

    第二次提交

--- testcase/test_department.py
+++ testcase/test_department.py
@@ -1,26 +1,24 @@
 import pytest,requests,json
 
-# def test_1_add_department(get_token_fixture):
-#     """增加部门信息"""
-#     # 通过Fixture函数获取get_token_fixture值，即token，再将token添加到请求头中
-#     headers = {
-#         "Content-Type": "application/json;charset=utf8",
-#         "Authorization": get_token_fixture
-#     }
-#     data = {
-#             "createTime": "",
-#             "sortNumber": 1,
-#             "name": "测试部门3",
-#             "checked": "true",
-#             "updateTime": "",
-#             "shortName": "测试部",
-#             "status": 1
-#     }
-#     url = "http://192.168.110.173:9000/sys-admin-api/v1/department"
-#     res = requests.post(url=url, headers=headers, json=data).text
-#     res = json.loads(res)
-#     print(res)
-#     assert res["code"] == 200
+def test_1_add_department(get_token_fixture):
+    """增加部门信息"""
+    # 通过Fixture函数获取get_token_fixture值，即token，再将token添加到请求头中
+    headers = {
+        "Content-Type": "application/json;charset=utf8",
+        "Authorization": get_token_fixture
+    }
+    data = {
+            "sortNumber": 1,
+            "name": "测试部门8-22",
+            "checked": "true",
+            "shortName": "测试部",
+            "status": 1
+    }
+    url = "http://192.168.110.173:9000/sys-admin-api/v1/department"
+    res = requests.post(url=url, headers=headers, json=data).text
+    res = json.loads(res)
+    print(res)
+    assert res["code"] == 200
 
 def test_2_select_department(get_token_fixture):
     """查询部门信息"""
---
+++ testcase/test_information.py
@@ -0,0 +1,58 @@
+import pytest,requests,json
+def test_1_add_information(get_token_fixture):
+    """新增消息"""
+    # 通过Fixture函数获取get_token_fixture值，即token，再将token添加到请求头中
+    headers = {
+        "Content-Type": "application/json;charset=utf8",
+        "Authorization": get_token_fixture
+    }
+    data = {
+        "read": 1,
+        "resType": 1,
+        "name": "纸巾",
+        "title": "新增订单消息",
+        "userId": 1,
+        "resId": 2,
+        "content": "你的订单已开始处理，等待商家发货",
+        "outInId": 3,
+        "status": 1
+    }
+    url = "http://192.168.110.173:9000/erp-admin-api/v1/sysMessage"
+    res = requests.post(url=url, headers=headers, json=data).text
+    res = json.loads(res)
+    print(res)
+    assert res["code"] == 200
+
+def test_2_select_information(get_token_fixture):
+    """查询消息详情"""
+    # 通过Fixture函数获取get_token_fixture值，即token，再将token添加到请求头中
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf8",
+        "Authorization": get_token_fixture
+    }
+    data = {"id":"9"}
+    url = f"http://192.168.110.173:9000/erp-admin-api/v1/sysMessage"
+    res = requests.get(url=url, headers=headers,params =data).text
+    res = json.loads(res)
+    print(res)
+    assert res["code"] == 200
+
+def test_3_select_page_information(get_token_fixture):
+    """查询消息详情"""
+    # 通过Fixture函数获取get_token_fixture值，即token，再将token添加到请求头中
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf8",
+        "Authorization": get_token_fixture
+    }
+    data = {"page":"1","limit":"10"}
+    url = f"http://192.168.110.173:9000/erp-admin-api/v1/sysMessage/page"
+    res = requests.get(url=url, headers=headers,params=data).text
+    res = json.loads(res)
+    print(res)
+    assert res["code"] == 200
+
+if __name__ == '__main__':
+    pytest.main()
+
+
+
